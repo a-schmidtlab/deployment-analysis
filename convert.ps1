eda276a89001bf162809ca26c089f34dd06915fb
a-schmidtlab <axel@schmidtlab.net>
2025-02-26 10:51:48 +0100

    clean up

--- convert.ps1
+++
@@ -1,1 +0,0 @@
-Set-Content -Path "dist\DeploymentAnalyzer-Perfect\DeploymentAnalyzer.exe" -Value (Get-Content -Path "dist\DeploymentAnalyzer-Perfect\DeploymentAnalyzer.vbs.txt" -Raw) -Encoding Byte 
